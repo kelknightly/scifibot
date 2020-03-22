6fcdf018b9865ab6244b6c34ad4bbe618c68355e
Kelly Quantrill <kelly@trigraphgroup.com>
2020-03-22 11:12:03 +0100

    Fixing titling error

--- bot_dev.py
+++ bot_dev.py
@@ -11,7 +11,7 @@ from scifisanity import scifisanity
 bot = scifisanity()
 
 # Print all the sentences the generator in scifisanity knows about
-#bot.print_all_sentences()
+bot.print_all_sentences()
 
 # Print just one sentence from one of the sentences registered with the generator inside scifisanity
-bot.print_sentence_by_name('Sentence 54')
+#bot.print_sentence_by_name('Sentence 54')
--- sentences/sentence21.py
+++ sentences/sentence21.py
@@ -3,7 +3,7 @@ from sentences.base_sentence import base_sentence
 class sentence21(base_sentence):
     
     def get_sentence(self, components):
-        sentence = 'Several ' + components['secondary_actor_desc'].title() 
+        sentence = 'Several ' + components['secondary_actor_desc']
         sentence += ' ' + components['secondary_actors'] 
         sentence += ' drift in empty space, much too far to be rescued. They are alone.'
 
