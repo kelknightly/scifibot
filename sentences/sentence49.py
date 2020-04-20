4c05cff16c9a07e33d8197ac06461b41a283cc48
Kelly Quantrill <kelly@trigraphgroup.com>
2020-04-20 12:14:48 +0200

    New transport damage options

--- sentences/sentence49.py
+++ sentences/sentence49.py
@@ -9,7 +9,7 @@ class sentence49(base_sentence):
         sentence += ' drifts helplessly in space. ' + components['pronoun'].title()
         sentence += ' is unconscious, and ' + components['pos_pro'] 
         sentence += ' ' + components['transport']
-        sentence += ' is ' + components['transport_damage_action']
+        sentence += ' ' + components['transport_damage_action']
         sentence += '.'
 
         return sentence
